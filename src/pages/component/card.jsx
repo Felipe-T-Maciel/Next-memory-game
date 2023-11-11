aad3147a4426d1a903724974dcd6d58e7409b959
Felipe Tomio Maciel <felipetomio@weg.net>
2023-11-10 21:56:46 -0300

    save

--- src/pages/component/card.jsx
+++ src/pages/component/card.jsx
@@ -1,14 +1,11 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 // import { useState } from "react"
 export default function Card(props) {
 
     return(
         <>
-            <div className="  card duration-1000 flex justify-center items-center p-6" style={{height:'40%', width:'17%', backgroundColor:'blue', borderRadius:'5%', cursor:'pointer'}} 
-            onClick={(e) => {
-                e.target.classList.toggle('flip')
-            }}>
+            <div className="  card duration-1000 flex select-none justify-center items-center p-6" style={{height:'40%', width:'17%', backgroundColor:'blue', borderRadius:'5%', cursor:'pointer'}}>
                 <div className="face back">{props.prop}</div>
             </div>
         </>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -22,11 +22,63 @@ export default function Home() {
       })
     const randomizeArray = cartasEmbaralhadas.sort(() => 0.5 - Math.random());
     addCarta(randomizeArray.slice(0, (gay.cartas.length*2)));
+
+    
+    
   }, []);
+
+  useEffect(()=>{
+    let cartas = document.querySelectorAll('.card')
+    let cartasVitoria = []
+    cartas.forEach(carta => {
+      carta.addEventListener("click",()=>{
+        if(verificaPodeJogar(carta)){
+          carta.classList.add('flip')
+          cartasVitoria.push(carta)
+          if(cartasVitoria.length==2){
+            if(verificaAcerto(cartasVitoria)){
+              console.log("igual")
+            }
+            cartasVitoria = []
+          }
+        }
+      })
+    })
+
+    function verificaAcerto(cartas){
+      if(cartas[0].innerText === cartas[1].innerText){
+        return true
+      }else{
+        
+        return false
+      }
+    }
+
+    function verificaPodeJogar(carta){
+      let i = 0 
+      cartas.forEach(e => {
+        if(e.classList.contains('flip') && e != carta){
+          i+=1
+        }
+      })
+      if(i>=1){
+        setTimeout(()=>{
+            cartas.forEach(e => {
+              e.classList.remove('flip')
+          })
+        },1000)
+        if(i==2){
+          i = 0
+          return false
+        }
+      }
+      return true
+    }
+  })
 
   return (
     <>
-      <div className="flex gap-[2%] flex-wrap p-[2%] justify-center h-screen items-center">
+      <div className="flex duration-1000 gap-[2%] flex-wrap p-[2%] justify-center h-screen items-center" >
         {cartasEmbaralhadas.map(carta => {
           return <Card prop = {carta} />
         })}
