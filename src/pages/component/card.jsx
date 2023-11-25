f23f9dbd21a8298ddfd8e7abebab8f98857c0830
`felipe-t-maciel` <`felipe_t_maciel@estudante.sesisenai.org.br`>
2023-11-25 19:58:06 -0300

    salvo

--- src/pages/component/card.jsx
+++ src/pages/component/card.jsx
@@ -5,8 +5,10 @@ export default function Card(props) {
 
     return(
         <>
-            <div className="  card ">
-                <div className="face back">{props.prop}</div>
+            <div className="  card w-[17%] h-[40%]  left-1 duration-1000">
+                <div className="face back h-full w-full ">
+                    <div className="h-full w-full rounded-xl bg-cover bg-[url('')] " style={{backgroundImage: `url(${props.prop})`}}></div>
+                </div>
             </div>
         </>
     )
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,34 +1,34 @@
 import Card from "@/pages/component/card"
 import { useEffect, useState } from "react"
 export default function Home() {
-  
+
   let gay = {
-    cartas : [
-      "gabriel",
-      "Felipe"
+    cartas: [
+      "https://imgv3.fotor.com/images/cover-photo-image/Astronaut-wearing-orange-suits-and-standing-on-the-the-planet.png",
+      "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8JTQwbWFufGVufDB8fDB8fHww&w=1000&q=80"
     ]
   }
   const [cartasEmbaralhadas, addCarta] = useState([])
   const [inicio, addNum] = useState(0)
 
   useEffect(() => {
-      gay.cartas.map(carta => {
-        const vet = cartasEmbaralhadas
-        vet.push(carta)
-        addCarta(vet)
-      })
+    gay.cartas.map(carta => {
+      const vet = cartasEmbaralhadas
+      vet.push(carta)
+      addCarta(vet)
+    })
     const randomizeArray = cartasEmbaralhadas.sort(() => 0.5 - Math.random());
-    addCarta(randomizeArray.slice(0, (gay.cartas.length*2)));
+    addCarta(randomizeArray.slice(0, (gay.cartas.length * 2)));
   }, []);
 
-  useEffect(()=>{
+  useEffect(() => {
     let cartas = document.querySelectorAll('.card')
-    if(inicio===0){
+    if (inicio === 0) {
       cartas.forEach(element => {
         element.classList.add('flip')
       });
 
-      setTimeout(()=>{
+      setTimeout(() => {
         cartas.forEach(element => {
           element.classList.remove('flip')
         });
@@ -37,11 +37,11 @@ export default function Home() {
     }
     let cartasVitoria = []
     cartas.forEach(carta => {
-      carta.addEventListener("click",()=>{
-        if(verificaPodeJogar(carta)){
+      carta.addEventListener("click", () => {
+        if (verificaPodeJogar(carta)) {
           carta.classList.add('flip')
           cartasVitoria.push(carta)
-          if(cartasVitoria.length==2){
+          if (cartasVitoria.length == 2) {
             verificaAcerto(cartasVitoria, cartas)
             cartasVitoria = []
           }
@@ -50,44 +50,37 @@ export default function Home() {
     })
 
     const verificaAcerto = (cartasVitoria) => {
-      if(cartasVitoria[0].innerText === cartasVitoria[1].innerText){
+      if (cartasVitoria[0].innerText === cartasVitoria[1].innerText) {
         let arrayTeste = []
         setTimeout(() => {
           for (let i = cartasEmbaralhadas.length; i >= 0; i--) {
-            if(cartasEmbaralhadas[i] === cartasVitoria[0].innerText || cartasEmbaralhadas[i] === cartasVitoria[1].innerText){
+            if (cartasEmbaralhadas[i] === cartasVitoria[0].innerText || cartasEmbaralhadas[i] === cartasVitoria[1].innerText) {
               cartasEmbaralhadas.splice(i, 1)
             }
           }
-          cartas.forEach(e=>{
-            console.log(e)
-            if(e === cartasVitoria[0].innerText || e === cartasVitoria[1].innerText){
-              e.classList.remove('card')
-              e.classList.add('card-remove')
-            }
-          })
           cartasEmbaralhadas.forEach(element => {
             arrayTeste.push(element)
           });
 
           addCarta(arrayTeste)
-        },1500)
+        }, 1500)
       }
     }
-  
+
     const verificaPodeJogar = (carta) => {
-      let i = 0 
+      let i = 0
       cartas.forEach(e => {
-        if(e.classList.contains('flip') && e != carta){
-          i+=1
+        if (e.classList.contains('flip') && e != carta) {
+          i += 1
         }
       })
-      if(i>=1){
-        setTimeout(()=>{
-            cartas.forEach(e => {
-              e.classList.remove('flip')
+      if (i >= 1) {
+        setTimeout(() => {
+          cartas.forEach(e => {
+            e.classList.remove('flip')
           })
-        },1000)
-        if(i==2){
+        }, 1000)
+        if (i == 2) {
           i = 0
           return false
         }
@@ -98,17 +91,30 @@ export default function Home() {
 
   return (
     <>
-      <div className="flex duration-1000 gap-[2%] flex-wrap p-[2%] justify-center h-screen items-center" onChange={()=>{
+       <div className="w-screen h-screen">
+       <div className="flex justify-center items-center h-[5vh]">
+          <p className="text-3xl">Memory Game</p>
+        </div>
+        <div className="grid grid-cols-3" style={{ gridTemplateColumns: '8% 84% 8%' }}>
+          <div className="col-start-1 col-end-2 flex justify-center items-center">
+            <p className="text-white text-4xl font-semibold">3</p>
+          </div>
+          <div className="col-start-2 col-end-3 flex duration-1000 gap-[2%] flex-wrap p-[2%] justify-center h-screen items-center" onChange={() => {
 
-      }} >
-        {
-        
-          cartasEmbaralhadas.map((carta) => {
-            return <Card prop = {carta} />
-          })
-        
-        }
-      </div>
+          }} >
+            {
+
+              cartasEmbaralhadas.map((carta) => {
+                return <Card prop={carta} />
+              })
+
+            }
+          </div>
+          <div className="col-start-3 flex justify-center items-center">
+            <p className="text-white text-4xl font-semibold">3</p>
+          </div>
+        </div>
+       </div>
     </>
   )
 }
