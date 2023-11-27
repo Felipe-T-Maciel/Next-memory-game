1fa6efb579f6e64070ae545b9e29244914da96b8
Felipe Tomio Maciel <felipetomio@weg.net>
2023-11-27 14:43:51 -0300

    save

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,107 +2,24 @@ import Card from "@/pages/component/card"
 import { useEffect, useState } from "react"
 export default function Home() {
 
-  let gay = {
-    cartas: [
-      "https://imgv3.fotor.com/images/cover-photo-image/Astronaut-wearing-orange-suits-and-standing-on-the-the-planet.png",   
-      "https://static-cse.canva.com/blob/759754/IMAGE1.jpg",
-      "https://designdobom.com.br/wp-content/uploads/2018/09/imagem_para_sexta_51.jpg",
-      "https://mariananardi.com.br/wp-content/uploads/2018/09/Gato-x-leao.jpg"
+   cartas: [
+      {
+        foto:"https://imgv3.fotor.com/images/cover-photo-image/Astronaut-wearing-orange-suits-and-standing-on-the-the-planet.png",
+        id: 0   
+      },
+      {
+        foto:"https://static-cse.canva.com/blob/759754/IMAGE1.jpg",
+        id: 1   
+      }
     ]
-  }
+
   const [cartasEmbaralhadas, addCarta] = useState([])
   const [inicio, addNum] = useState(0)
   const [contador, addCont] = useState(0)
   const [pontoJ1, addJ1] = useState(0)
   const [pontoJ2, addJ2] = useState(0)
 
-  useEffect(() => {
-    gay.cartas.map(carta => {
-      const vet = cartasEmbaralhadas
-      vet.push(carta)
-      addCarta(vet)
-    })
-    const randomizeArray = cartasEmbaralhadas.sort(() => 0.5 - Math.random());
-    addCarta(randomizeArray.slice(0, (gay.cartas.length * 2)));
-  }, []);
 
-  useEffect(() => {
-    let cartas = document.querySelectorAll('.card')
-    if (inicio === 0) {
-      cartas.forEach(element => {
-        element.classList.add('flip')
-      });
-
-      setTimeout(() => {
-        cartas.forEach(element => {
-          element.classList.remove('flip')
-        });
-      }, 1000)
-      addNum(1)
-    }
-    let cartasVitoria = []
-    cartas.forEach(carta => {
-      carta.addEventListener("click", () => {
-        if (verificaPodeJogar(carta)) {
-          carta.classList.add('flip')
-          cartasVitoria.push(carta)
-          if (cartasVitoria.length == 2) {
-            if(verificaAcerto(cartasVitoria)){
-              console.log('entrei')
-              if(contador!=2==0){
-                addJ1(pontoJ1+1)
-              }else{
-                addJ1(pontoJ2+1)
-              }
-            }
-            cartasVitoria = []
-            addCont(contador+1)
-          }
-        }
-      })
-    })
-
-    const verificaAcerto = (cartasVitoria) => {
-      if (cartasVitoria[0].lastChild.lastChild.style.backgroundImage === cartasVitoria[1].lastChild.lastChild.style.backgroundImage) {
-        let arrayTeste = []
-        setTimeout(() => {
-          for (let i = cartasEmbaralhadas.length; i >= 0; i--) {
-            if (cartasEmbaralhadas[i] === cartasVitoria[0].lastChild.lastChild.style.backgroundImage.slice(5, -2) || cartasEmbaralhadas[i] === cartasVitoria[1].lastChild.lastChild.style.backgroundImage.slice(5, -2)) {
-              cartasEmbaralhadas.splice(i, 1)
-            }
-          }
-          cartasEmbaralhadas.forEach(element => {
-            arrayTeste.push(element)
-          });
-
-          addCarta(arrayTeste)
-        }, 1500)
-        return true
-      }
-      return false
-    }
-
-    const verificaPodeJogar = (carta) => {
-      let i = 0
-      cartas.forEach(e => {
-        if (e.classList.contains('flip') && e != carta) {
-          i += 1
-        }
-      })
-      if (i >= 1) {
-        setTimeout(() => {
-          cartas.forEach(e => {
-            e.classList.remove('flip')
-          })
-        }, 1000)
-        if (i == 2) {
-          i = 0
-          return false
-        }
-      }
-      return true
-    }
-  })
 
   return (
     <>
@@ -119,7 +36,7 @@ export default function Home() {
           }} >
             {
 
-              cartasEmbaralhadas.map((carta, index) => {
+              cartasEmbaralhadas.map((cartas, index) => {
                 return <Card key={index} prop={carta} />
               })
 
