58df51940d2f04d0e24a8527c013ea2ef410be91
FELIPE TOMIO MACIEL <felipe_t_maciel@estudante.sesisenai.org.br>
2023-11-30 09:28:13 -0300

    cabei

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -20,6 +20,7 @@ export default function Home() {
   const [contador, addCont] = useState(0)
   const [pontoJ1, addJ1] = useState(0)
   const [pontoJ2, addJ2] = useState(0)
+  const [jogadorAtual, set] = useState('')
 
   const embaralharCartas = () => {
     const cartasDuplicadas = [...gay.cartas,...gay.cartas];
@@ -36,6 +37,7 @@ export default function Home() {
 
   useEffect(() => {
     embaralharCartas()
+    mudaJogador()
   }, [])
 
   useEffect(() => {
@@ -53,22 +55,26 @@ export default function Home() {
       addNum(1)
     }
     let cartasVitoria = []
+    let i = 0
     cartas.forEach(carta => {
       carta.addEventListener("click", () => {
         if (verificaPodeJogar(carta)) {
           carta.classList.add('flip')
           cartasVitoria.push(carta)
-          if (cartasVitoria.length == 2) {
-            if(verificaAcerto(cartasVitoria)){
-              console.log('entrei')
-              if(contador!=2==0){
+          
+          if (cartasVitoria.length == 2 && i === 0) {
+            if(verificaAcerto(cartasVitoria) && i === 0){
+              if(contador%2===0 && i === 0){
                 addJ1(pontoJ1+1)
               }else{
-                addJ1(pontoJ2+1)
+                addJ2(pontoJ2+1)
               }
             }
+            if(i===0){
+              i=1
+            }
+            mudaJogador()
             cartasVitoria = []
-            addCont(contador+1)
           }
         }
       })
@@ -78,9 +84,11 @@ export default function Home() {
       if (cartasVitoria[0].lastChild.lastChild.style.backgroundImage === cartasVitoria[1].lastChild.lastChild.style.backgroundImage) {
           setTimeout(() => {
             cartasVitoria[0].classList.add('invisible')
+            cartasVitoria[0].classList.add('block')
+            cartasVitoria[1].classList.add('block')
             cartasVitoria[1].classList.add('invisible')
-            return true
           }, 500);
+          return true
       }
       return false
     }
@@ -88,7 +96,7 @@ export default function Home() {
     const verificaPodeJogar = (carta) => {
       let i = 0
       cartas.forEach(e => {
-        if (e.classList.contains('flip') && e != carta) {
+        if (e.classList.contains('flip') && e != carta && !e.classList.contains('invisible')) {
           i += 1
         }
       })
@@ -107,17 +115,26 @@ export default function Home() {
     }
   })
 
+  const mudaJogador = () => {
+    if(contador%2==0){
+      set('Player 2')
+    }else{
+      set('Player 1')
+    }
+    addCont(contador+1)
+  }
+
   return (
     <>
        <div className="w-screen h-screen bg-[url('https://i.pinimg.com/originals/8e/46/15/8e46150f790fbefe438d9c2767c32ad1.gif')]">
        <div className="flex justify-center items-center gap-[10%] ml-[20%] h-[5vh]">
           <p className="text-gray-300 text-3xl font-semibold">Mario Game</p>
-          <p className="text-gray-300 text-lg font-semibold">Jogador atual: </p>
+          <p className="text-gray-300 text-lg font-semibold">Jogador atual: {jogadorAtual}</p>
         </div>
         <div className="grid grid-cols-3" style={{ gridTemplateColumns: '8% 84% 8%' }}>
           <div className="col-start-1 col-end-2 flex flex-col justify-center items-center">
             <p className="text-gray-300 text-2xl font-semibold">Player-1</p>
-            <p className="text-gray-300 text-2xl font-semibold">Earns: </p>
+            <p className="text-gray-300 text-2xl font-semibold">Earns: {pontoJ1}</p>
           </div>
           <div className="col-start-2 col-end-3 flex duration-1000 gap-[0.8%] flex-wrap justify-center h-screen items-center">
             {cartasEmbaralhadas.map((carta) => (
@@ -128,7 +145,7 @@ export default function Home() {
           </div>
           <div className="col-start-3 flex flex-col justify-center items-center">
             <p className="text-gray-300 text-2xl font-semibold">Player-2</p>
-            <p className="text-gray-300 text-lg font-semibold">Earns: </p>
+            <p className="text-gray-300 text-lg font-semibold">Earns: {pontoJ2}</p>
           </div>
         </div>
        </div>
