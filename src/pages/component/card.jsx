93d9340f56db51da2043ef74a13504a351399951
`felipe-t-maciel` <`felipe_t_maciel@estudante.sesisenai.org.br`>
2023-11-30 01:17:50 -0300

    vsf henrique

--- src/pages/component/card.jsx
+++ src/pages/component/card.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react"
-export default function Card({prop, onClick}) {
 
+// import { useState } from "react"
+export default function Card(prop) {
     return(
         <>
-            <div className=" card w-[10%] h-[30%] bg-center  left-1 duration-1000 bg-cover bg-[url('/gordo.jpeg')] " onClick={onClick}>
+            <div className=" card w-[10%] h-[30%] bg-center  left-1 duration-1000 bg-cover bg-[url('/gordo.jpeg')] ">
                 <div className="face back h-full w-full ">
-                    <div className="h-full w-full rounded-xl bg-cover bg-center " style={{backgroundImage: `url(${prop})`}}></div>
+                    <div className="h-full w-full rounded-xl bg-cover bg-center " style={{backgroundImage: `url(${prop.prop})`}}></div>
                 </div>
             </div>
         </>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,42 +2,27 @@ import Card from "@/pages/component/card"
 import { useEffect, useState } from "react"
 export default function Home() {
 
-   const cartas = [
-    {
-      foto:
-        "https://imgv3.fotor.com/images/cover-photo-image/Astronaut-wearing-orange-suits-and-standing-on-the-the-planet.png",
-      id: 1,
-    },
-    {
-      foto: "https://static-cse.canva.com/blob/759754/IMAGE1.jpg",
-      id: 2,
-    },
-    {
-      foto: "https://kinsta.com/wp-content/uploads/2020/08/tiger-jpg.jpg",
-      id: 3,
-    },
-    {
-      foto: "https://s1.static.brasilescola.uol.com.br/be/conteudo/images/imagem-em-lente-convexa.jpg",
-      id: 4,
-    },
-    {
-      foto: "https://img.freepik.com/fotos-gratis/close-na-renderizacao-3d-da-aguia_23-2150949851.jpg",
-      id: 5,
-    }
-  ];
-
+  let gay = {
+    cartas: [
+      "a.jpeg",
+      "b.jpeg",
+      "c.jpeg",
+      "d.jpeg",
+      "e.jpeg",
+      "f.jpeg",
+      "g.jpeg",
+      "h.jpeg",
+      "i.jpeg"
+    ]
+  }
   const [cartasEmbaralhadas, setCartasEmbaralhadas] = useState([])
+  const [inicio, addNum] = useState(0)
   const [contador, addCont] = useState(0)
   const [pontoJ1, addJ1] = useState(0)
   const [pontoJ2, addJ2] = useState(0)
-  const [jogadorAtual, set] = useState('')
-
-  useEffect(() => {
-    embaralharCartas()
-  }, [])
 
   const embaralharCartas = () => {
-    const cartasDuplicadas = [...cartas,...cartas];
+    const cartasDuplicadas = [...gay.cartas,...gay.cartas];
     for (let i = cartasDuplicadas.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [cartasDuplicadas[i], cartasDuplicadas[j]] = [
@@ -50,109 +35,100 @@ export default function Home() {
   };
 
   useEffect(() => {
-    console.clear()
-    if(contador%2===0){
-      set('player 1')
-    }else{
-      set('player 2')
-    }
-  })
+    embaralharCartas()
+  }, [])
 
-  const viraCartas = () => {
-    const cards = document.querySelectorAll('.card')
-    setTimeout(() => {
-      cards.forEach((card) => {
-        if(card.classList.contains('flip')){
-            card.classList.remove('flip')
-        }
-      })
-    }, 1500);
-  }
-
-  let carta1 = ''
-  let carta2 = ''
   useEffect(() => {
-    const cards = document.querySelectorAll('.card')
-    cards.forEach((card) => {
-      card.addEventListener('click', () => {
-        card.classList.add('flip')
-        if(carta1 === ''){
-          carta1 = card.lastChild.lastChild.style.backgroundImage.slice(5,-2)
-        }else{
-          carta2 = card.lastChild.lastChild.style.backgroundImage.slice(5,-2)
-        }
+    let cartas = document.querySelectorAll('.card')
+    if (inicio === 0) {
+      cartas.forEach(element => {
+        element.classList.add('flip')
+      });
 
-        if(carta1 != '' && carta2 != ''){
-          if(carta1 === carta2){
-            verificaAcerto(cards)
+      setTimeout(() => {
+        cartas.forEach(element => {
+          element.classList.remove('flip')
+        });
+      }, 1000)
+      addNum(1)
+    }
+    let cartasVitoria = []
+    cartas.forEach(carta => {
+      carta.addEventListener("click", () => {
+        if (verificaPodeJogar(carta)) {
+          carta.classList.add('flip')
+          cartasVitoria.push(carta)
+          if (cartasVitoria.length == 2) {
+            if(verificaAcerto(cartasVitoria)){
+              console.log('entrei')
+              if(contador!=2==0){
+                addJ1(pontoJ1+1)
+              }else{
+                addJ1(pontoJ2+1)
+              }
+            }
+            cartasVitoria = []
+            addCont(contador+1)
           }
-          viraCartas()
-          carta1 = ''
-          carta2 = ''
         }
       })
     })
-  })
 
-  function verificaAcerto(cards){
-    cards.forEach(element => {
-      if(element.lastChild.lastChild.style.backgroundImage.slice(5,-2) === carta1){
-        element.classList.add('invisible')
-        console.log(contador);
-        if(contador%2===0){
-          addJ1(pontoJ1+1)
-        }else{
-          addJ2(pontoJ2+1)
+    const verificaAcerto = (cartasVitoria) => {
+      if (cartasVitoria[0].lastChild.lastChild.style.backgroundImage === cartasVitoria[1].lastChild.lastChild.style.backgroundImage) {
+          setTimeout(() => {
+            cartasVitoria[0].classList.add('invisible')
+            cartasVitoria[1].classList.add('invisible')
+            return true
+          }, 500);
+      }
+      return false
+    }
+
+    const verificaPodeJogar = (carta) => {
+      let i = 0
+      cartas.forEach(e => {
+        if (e.classList.contains('flip') && e != carta) {
+          i += 1
+        }
+      })
+      if (i >= 1) {
+        setTimeout(() => {
+          cartas.forEach(e => {
+            e.classList.remove('flip')
+          })
+        }, 1000)
+        if (i == 2) {
+          i = 0
+          return false
         }
       }
-    });
-    verificaSeAcabou(cards)
-  }
-
-  function verificaSeAcabou(cards){
-    let contador = 0
-    cards.forEach(element => {
-      if(element.classList.contains('invisible')){
-        contador+=1
-      }
-    });
-    if(contador === cards.length-1){
       return true
     }
-  }
-
-  let cont = 0
-  const addContador = () => {
-    cont+=1
-    if(cont==2){
-      addCont(contador+1)
-      cont = 0
-    }
-  }
+  })
 
   return (
     <>
        <div className="w-screen h-screen bg-[url('https://i.pinimg.com/originals/8e/46/15/8e46150f790fbefe438d9c2767c32ad1.gif')]">
        <div className="flex justify-center items-center gap-[10%] ml-[20%] h-[5vh]">
-          <p className="text-gray-300 text-3xl font-semibold">Henrique Game</p>
-          <p className="text-gray-300 text-lg font-semibold">Jogador atual: {jogadorAtual}</p>
+          <p className="text-gray-300 text-3xl font-semibold">Mario Game</p>
+          <p className="text-gray-300 text-lg font-semibold">Jogador atual: </p>
         </div>
         <div className="grid grid-cols-3" style={{ gridTemplateColumns: '8% 84% 8%' }}>
           <div className="col-start-1 col-end-2 flex flex-col justify-center items-center">
             <p className="text-gray-300 text-2xl font-semibold">Player-1</p>
-            <p className="text-gray-300 text-2xl font-semibold">Earns: {pontoJ1}</p>
+            <p className="text-gray-300 text-2xl font-semibold">Earns: </p>
           </div>
           <div className="col-start-2 col-end-3 flex duration-1000 gap-[0.8%] flex-wrap justify-center h-screen items-center">
             {cartasEmbaralhadas.map((carta) => (
-              <Card
-                prop={carta.foto}
-                onClick={() => addContador()}
+               <Card
+                prop={carta}
               />
             ))}
           </div>
           <div className="col-start-3 flex flex-col justify-center items-center">
             <p className="text-gray-300 text-2xl font-semibold">Player-2</p>
-            <p className="text-gray-300 text-lg font-semibold">Earns: {pontoJ2}</p>
+            <p className="text-gray-300 text-lg font-semibold">Earns: </p>
           </div>
         </div>
        </div>
